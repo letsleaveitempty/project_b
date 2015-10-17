ddf183506481b1b3679257728ac499d881492722
Jessica <jess.10@web.de>
2015-10-17 19:15:03 +0200

    facebook sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root 'landing#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+  end
   resources :achievements
   devise_for :users
 
