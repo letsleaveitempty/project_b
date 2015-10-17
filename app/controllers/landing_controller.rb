13ff82173d01d067a4a793ab2edc7d0e8126b706
letsleaveitempty <anita.jaszewska@gmail.com>
2015-10-17 18:30:18 +0200

    resolved routes issues and redirect after sign_up

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,9 @@
 class LandingController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
+    if current_user
+      redirect_to achievements_path
+    end
   end
 end
